2a6711b0966abed716e7ce2e2072d2008c0cae04
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-04 18:29:59 +0530

    MyPlacement goal

---
+++ track/M01-understanding-java/T02-evaluation-of-programming/MyPlacementGoal.java
@@ -0,0 +1,8 @@
+public class MyPlacementGoal {
+    public static void main(String[] args){
+        System.out.println("---My Placement Goal----");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: GEt Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
