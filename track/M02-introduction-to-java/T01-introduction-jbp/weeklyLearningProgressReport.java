2ae86c97edf7cd0c95f37052024fd515dd72c57c
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-04 19:47:40 +0530

    weeklyLearningProgressReport

---
+++ track/M02-introduction-to-java/T01-introduction-jbp/weeklyLearningProgressReport.java
@@ -0,0 +1,13 @@
+public class weeklyLearningProgressReport {
+    public static void main(String[] args) {
+        String name = "Haseena";
+        int javaTopicsCompleted = 5;
+        int aptitudeTopicsCompleted = 3;
+        int mockTestTaken = 2;
+
+        System.out.println("Name: " + name);
+        System.out.println("Java Topics Completed: " + javaTopicsCompleted);
+        System.out.println("Aptitude Topics Completed: " + aptitudeTopicsCompleted);
+        System.out.println("Mock Test Taken: " + mockTestTaken);
+    }
+}
