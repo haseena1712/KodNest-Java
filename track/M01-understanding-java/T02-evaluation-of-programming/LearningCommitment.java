434acaca7389c91b36005572b779987f176def00
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-04 18:24:48 +0530

    LearningCommitment

---
+++ track/M01-understanding-java/T02-evaluation-of-programming/LearningCommitment.java
@@ -0,0 +1,6 @@
+public class LearningCommitment{
+    public static void main(String[] args){
+        System.out.println("Track: java Development");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
