a79f777116e2f03a4f74d291c264786799ee4748
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-04 19:03:31 +0530

    StudeentIdCard

--- track/M01-understanding-java/T02-evaluation-of-programming/StudentIdCard.java
+++ track/M01-understanding-java/T02-evaluation-of-programming/StudentIdCard.java
@@ -1,7 +1,7 @@
 public class StudentIdCard {
     public static void main(String[] args){
          System.out.println("-----------------------");
-        System.out.println("Student Profile:");
+        System.out.println("Student Profile");
         System.out.println("Name:  Hemanth ");
         System.out.println("Course: Java");
         System.out.println("Level: Beginner");
