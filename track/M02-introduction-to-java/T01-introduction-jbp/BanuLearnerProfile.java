2a2942d387cb729661a3d593ed18c7339b6bfdbc
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-04 19:39:33 +0530

    BanuLearnerProfile

---
+++ track/M02-introduction-to-java/T01-introduction-jbp/BanuLearnerProfile.java
@@ -0,0 +1,15 @@
+public class BanuLearnerProfile {
+    public static void main(String[] args) {
+        String name = "Banu";
+        String gender = "Female";
+        int age = 22;
+        String stream = "IT";
+        double cgpa = 8.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Stream: " + stream);
+        System.out.println("CGPA: " + cgpa);
+    }
+}
