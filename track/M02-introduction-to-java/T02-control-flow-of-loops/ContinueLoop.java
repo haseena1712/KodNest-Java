6b99d421dde494cd5d9d9d4a2b658f1ccae0033d
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-06 12:24:27 +0530

    ContinueLoop

---
+++ track/M02-introduction-to-java/T02-control-flow-of-loops/ContinueLoop.java
@@ -0,0 +1,10 @@
+public class ContinueLoop {
+    public static void main(String[] args){
+        for(int i=1 ; i<=20 ; i++){
+            if(i == 10){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
