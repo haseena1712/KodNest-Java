c8c41367e362ae3620d126631db230521d199796
Haseena Banu <shaikbanu1712@gmail.com>
2026-08-17 12:56:05 +0530

    pgm4

---
+++ track/M02-introduction-to-java/Array/pgm4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class pgm4 { 
+    public static void main(String[] argd) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a[] = new int[5];
+
+        System.out.println("Enter the array elements: ");
+        
+        for ( int i=0; i<=a.length-1; i++)
+        {
+             a[i] = scan.nextInt();
+        }
+
+        System.out.println("Array Elements are: ");
+
+        for ( int i=a.length-1; i>=0; i--)
+        {
+            System.out.println(a[i]);
+        }    
+    }
+}
